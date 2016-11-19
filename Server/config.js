a42df4a710ae191cdf8649e14356812037ec831c
johnathanlund <jalshield@gmail.com>
2016-11-18 18:42:39 -0700

    Upgrading to angular 2 for this project.

--- Server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: 3000,
-  MONGO_URI: 'mongodb://localhost:27017/handyman',
-  SESSION_SECRET: 'xiasyyuiieeeiiizxzxobionekonobi'
-};
---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'handyman-app',
+  template: '<h1>Told Handyman App!</h1>'
+})
+export class AppComponent { }
