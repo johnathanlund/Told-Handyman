3f10a6bcaa402d53245c7438ae9edb5c6ce7ce3b
johnathanlund <jalshield@gmail.com>
2017-02-20 18:56:17 -0700

    created file upload at server, working with postman

--- server/server.js
+++ server/server.js
@@ -3,7 +3,21 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var multer = require('multer');
+var fs = require('fs');
 // var path = require('path');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './server/uploads/');
+  },
+  filename: function (req, file, cb) {
+    var datetimestamp = Date.now();
+    cb(null, file.originalname + '-' + datetimestamp);
+  }
+});
+
+var upload = multer({storage: storage}).single('file');
 
 // CONFIG
 var config = require('./config');
@@ -35,7 +49,44 @@ app.use(cors());
 // app.use(passport.session());
 mongoose.set('debug', true);
 
+// HEADERS
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://valor-software.github.io');
+//   res.setHeader('Access-Control-Allow-Methods', 'POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+// MULTER SETTINGS
+// app.use(multer({
+//   dest: DIR,
+//   rename: function (fieldname, filename) {
+//     return filename + Date.now();
+//   },
+//   onFileUploadStart: function (file) {
+//     console.log(file.originalname + ' is starting ...');
+//   },
+//   onFileUploadComplete: function (file) {
+//     console.log(file.fieldname + ' uploaded to  ' + file.path + ' is complete!');
+//   }
+// }));
+
 // ENDPOINTS
+
+//===========Upload Endpoints=========================================
+app.get('/upload', function (req, res) {
+  res.end('File catcher example');
+});
+app.post('/upload', function (req, res) {
+  upload(req, res, function (err) {
+    console.log('This is the file being uploaded on server. req.file:  ' + req.filename);
+    if (err) {
+      return res.end(err.toString());
+    }
+    res.end('File is uploaded');
+  });
+});
 //===========User Endpoints============================================
 
 
