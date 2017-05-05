d376c7b9c1ef92d5ed6875b6ebc87d76dc87c5dd
johnathanlund <jalshield@gmail.com>
2017-05-04 20:39:20 -0600

    Working on creating AOT compiling.

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+import { platformBrowser }    from '@angular/platform-browser';
+import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
+console.log('Running AOT compiled');
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
