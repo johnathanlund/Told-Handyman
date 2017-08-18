f4639e1aaa6319db9d8bc7b2e1ba31d597d0ef97
johnathanlund <jalshield@gmail.com>
2017-08-18 16:26:21 -0600

    Successfull in server for user login to work with mysql.

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,17 +1,3 @@
-// var User = require('../models/UserModel.js');
-// var jwt = require('jsonwebtoken');
-// var config = require('../config');
-// var express = require('express');
-// var app = express();
-// var router = express.Router();
-// var auth = require('../services/auth');
-
-// app.use('/register', User.register);
-// app.use('/login', User.login);
-
-// router.post('/register', User.register);
-// router.post('/login', User.login);
-
 var User = require('../models/UserModel.js');
 var jwt = require('jsonwebtoken');
 var config = require('../config');
@@ -31,14 +17,19 @@ router.get('/check-state', auth.verifyToken, (req, res) => {
 });
 
 var generateHash = function( password ) {
-  console.log('In UserCtrl, generateHash');
+  // console.log('In UserCtrl, generateHash');
     return bcrypt.hashSync( password, bcrypt.genSaltSync(8), null);
 };
 
-var validPassword = function( password, res, err ) {
-  console.log('Password has been verified, in UserCtrl.');
-    return bcrypt.compareSync( password, this.password );
-  };
+// var validPassword = function( password, res, err ) {
+//   if (err) {
+//     console.log('Error occured for validPassword in userCtrl: '+ err);
+//     res.status(500).send(err);
+//   } else {
+//     console.log('Password has been verified, in UserCtrl.');
+//       return bcrypt.compareSync( password, this.password );
+//   }
+//   };
 
 router.post('/register', (req, res) => {
 
@@ -52,11 +43,7 @@ router.post('/register', (req, res) => {
        "created":today,
        "modified":today
      };
-     console.log('Password is: '+JSON.stringify(users.password));
-    //  users.password = User.generateHash(users.password);
     users.password = generateHash(users.password);
-     console.log('Now password is: '+JSON.stringify(users.password));
-     console.log('Inside UserCtrl for register: ' + JSON.stringify(users));
 
       db.query('insert into users set ?',users, function (error, results, fields) {
         if (error) {
@@ -66,8 +53,6 @@ router.post('/register', (req, res) => {
         "failed":"error ocurred"
       })
     }else{
-      console.log('The solution is: ', results);
-      console.log('right before res.send in Register.');
       // res.send({
       //   "code":200,
       //   "success":"user registered sucessfully"
@@ -81,7 +66,7 @@ router.post('/register', (req, res) => {
         message: 'You created a new user',
         token: token
       };
-      console.log('In register func for nextTick, content shows: '+JSON.stringify(content));
+      console.log('Successfully Registered new user "' + content.user.name + '" in userCtrl Register function.');
       res.send(content);
     }
       });
@@ -141,44 +126,103 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-
-  var reqUser = req.body;
-
-  User.findOne({'email' : reqUser.email}, (err, user) => {
-
-    if( err )
-      return done(err);
-
-    if( !user ) {
-      let content = {
-        success: false,
-        message: 'User does not exists'
-      };
-      res.send(content);
-      return;
+  var users = {
+       "name":req.body.name,
+       "email":req.body.email,
+       "password":req.body.password
+     };
+  var validPassword = function( password, res, err ) {
+    if (err) {
+      console.log('Error occured for validPassword in userCtrl: '+ err);
+      res.status(500).send(err);
+    } else {
+      console.log('In validPassword, password shows as: ',password);
+      console.log('In validPassword, users.password shows as: ',users.password);
+      console.log('Password has been verified, in UserCtrl.');
+        return bcrypt.compareSync( users.password, password);
     }
-
-    if( !user.validPassword(reqUser.password) ){
-      let content = {
-        success: false,
-        message: 'Incorrect password'
-      };
-      res.send(content);
-      return;
-    }
-
-    let token = jwt.sign(user, config.jwt_secret, {
-      expiresIn : 60*60*24
-    });
-    let content = {
-      user: user,
-      success: true,
-      message: 'You logged in',
-      token: token
     };
-    res.send(content);
 
-  })
+  // var reqUser = req.body;
+
+  db.query('SELECT * FROM users WHERE email = ?',[users.email], function (error, results, fields) {
+    console.log('THis is the rows from userCtrl Login func: ',results);
+    // var resultsPass = [0].password;
+    // console.log('1 resultsPass shows as: ' + rows[0]);
+    // console.log('2 resultsPass shows as: ' + JSON.stringify(rows[0].password));
+    if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    // console.log('The solution is: ', results);
+    if(results.length >0){
+      // console.log('In the IF for results. now shows pass as: ',results.RowDataPacket.password);
+      if(validPassword(results[0].password)){
+        console.log('Past the validPassword if statement.');
+        let token = jwt.sign(users, config.jwt_secret, {
+          expiresIn : 60*60*24
+        });
+        let content = {
+          user: users,
+          success: true,
+          message: 'You logged in',
+          token: token
+        };
+        res.send(content);
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email and password does not match"
+            });
+      }
+    }
+    else{
+      res.send({
+        "code":204,
+        "success":"Email does not exits"
+          });
+    }
+  }
+  });
+
+  // User.findOne({'email' : reqUser.email}, (err, user) => {
+  //
+  //   if( err )
+  //     return done(err);
+  //
+  //   if( !user ) {
+  //     let content = {
+  //       success: false,
+  //       message: 'User does not exists'
+  //     };
+  //     res.send(content);
+  //     return;
+  //   }
+  //
+  //   if( !user.validPassword(reqUser.password) ){
+  //     let content = {
+  //       success: false,
+  //       message: 'Incorrect password'
+  //     };
+  //     res.send(content);
+  //     return;
+  //   }
+  //
+  //   let token = jwt.sign(user, config.jwt_secret, {
+  //     expiresIn : 60*60*24
+  //   });
+  //   let content = {
+  //     user: user,
+  //     success: true,
+  //     message: 'You logged in',
+  //     token: token
+  //   };
+  //   res.send(content);
+  // })
 
 });
 
